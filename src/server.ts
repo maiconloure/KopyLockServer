2a0c0892492668db4856481da398b398bd635c50
Maicon Lourenço <maiconloure@gmail.com>
2023-01-25 21:44:12 -0300

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import fastify from "fastify"
+import cors from "@fastify/cors"
+import { appRoutes } from "./routes"
+
+const app = fastify()
+
+app.register(cors, {
+  origin: ['http://localhost:5555']
+})
+
+app.register(appRoutes)
+
+app.listen({
+  port: 3333
+}).then(() => {
+  console.log('HTTP Server running . . .')
+})
